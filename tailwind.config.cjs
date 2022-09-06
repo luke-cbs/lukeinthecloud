f113e581e191a7a246f5bbccb5a51af6c6f05fb2
luker <luke@coldbrewstudios.co.uk>
2022-09-06 22:36:19 +0100

    feature: added in base work for homepage

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'raleway': ['Raleway', 'sans-serif'],
+				'fira': ['Fira Code', 'sans-serif'],
+			}
+		},
 	},
 	plugins: [],
 }
