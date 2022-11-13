bd3d7665dadad0ed3e3b37525769f6ea50d9ead2
Luke Babich <luke@coldbrewstudios.co.uk>
2022-11-13 15:58:56 +0000

    cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify/functions";
 
-import image from "@astrojs/image";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()],
+  integrations: [tailwind()],
   output: 'server',
   adapter: netlify()
 });
